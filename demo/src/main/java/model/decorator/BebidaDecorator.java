f495008e369384292b58711dc8ba76523740700d
Izukill <luanloreto639@gmail.com>
2025-04-30 20:58:39 -0300

    feat: Criar Classe Abstrata BebidaDecorator

--- demo/src/main/java/model/decorator/BebidaDecorator.java
+++ demo/src/main/java/model/decorator/BebidaDecorator.java
@@ -1,4 +1,19 @@
-package model.decorator;
+package main.java.model.decorator;
+import model.bebida.Bebida;
 
-public class BebidaDecorator {
+public abstract class BebidaDecorator {
+    protected Bebida bebida;
+
+    public BebidaDecorator(Bebida bebida) {
+        this.bebida = bebida;
+    }
+
+
+
+    public abstract int getPreco(int x);
+
+    public abstract void getDescricao();
+
+
+
 }
