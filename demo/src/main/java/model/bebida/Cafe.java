69768453e03b7ee64e000cc4b4e433db99a5271d
Rosenilda Santos <119326136+rose-del@users.noreply.github.com>
2025-04-30 21:23:32 -0300

    feat: Update Cafe.java

--- demo/src/main/java/model/bebida/Cafe.java
+++ demo/src/main/java/model/bebida/Cafe.java
@@ -1,12 +1,12 @@
 package main.java.model.bebida;
+import model.bebida.Bebida;
 
 public class Cafe implements Bebida {
-    @Override
+
     public String getDescricao() {
         return "Café";
     }
 
-    @Override
     public double getPreco() {
         return 5.00;
     }
