8f063954aaa684d6b1caa8166c97b1cdb32402b6
rose-del <rosenildasantos.estudante@gmail.com>
2025-04-30 22:05:18 -0300

    feat: modificar classe Cafe

--- demo/src/main/java/model/bebida/Cafe.java
+++ demo/src/main/java/model/bebida/Cafe.java
@@ -1,11 +1,12 @@
 package model.bebida;
 
 public class Cafe implements Bebida {
-
+    @Override
     public String getDescricao() {
         return "Café";
     }
 
+    @Override
     public double getPreco() {
         return 5.00;
     }
